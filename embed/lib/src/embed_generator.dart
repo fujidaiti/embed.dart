6d35c5947f8fbcf5c043dd1eb4dac2cde8926748
fujidaiti <fujidaiti@gmail.com>
2023-07-12 09:51:08 +0900

    change: Restrict annotatable elements to top level variables only

--- embed/lib/src/embed_generator.dart
+++ embed/lib/src/embed_generator.dart
@@ -11,10 +11,19 @@ import 'package:source_gen/source_gen.dart';
 class EmbedGenerator extends GeneratorForAnnotation<Embed> {
   @override
   FutureOr<String> generateForAnnotatedElement(
-    Element element,
-    ConstantReader annotation,
-    BuildStep buildStep,
-  ) async {
+      Element element, ConstantReader annotation, BuildStep buildStep) {
+
+    if (element.kind != ElementKind.TOP_LEVEL_VARIABLE) {
+      throw ArgumentError.value(element, "element",
+          "Only top level variables can be annotated with ${Embed}s");
+    }
+
+    return _run(element, annotation, buildStep);
+  }
+
+  Future<String> _run(
+      Element element, ConstantReader annotation, BuildStep buildStep) async {
+
     final embedder = resolveEmbedder(annotation);
     final contentPath = resolvePath(embedder.config.path, buildStep.inputId);
     final content = resolveContent(contentPath);
