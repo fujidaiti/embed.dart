c885b01fb2269a994cf1762d81f0327fc5c6430e
fujidaiti <fujidaiti@gmail.com>
2023-07-24 12:41:22 +0900

    add: example for pub points

---
+++ embed/example/bin/example.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+import 'package:embed_annotation/embed_annotation.dart';
+
+@EmbedLiteral("/pubspec.yaml")
+var pubspec;
+
+void main() {}
