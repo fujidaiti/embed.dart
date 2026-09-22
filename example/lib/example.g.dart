c7ca863246cc4fd6573ddbae696c4c50bdbbd541
fujidaiti <fujidaiti@gmail.com>
2026-09-23 06:08:23 +0900

    track generated code

---
+++ example/lib/example.g.dart
@@ -0,0 +1,438 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'example.dart';
+
+// **************************************************************************
+// StrEmbeddingGenerator
+// **************************************************************************
+
+const _$embeddedText = r'''
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Fusce lorem ante, molestie eu facilisis sed, semper ut odio. 
+Vestibulum nulla nibh, elementum ac imperdiet at, vulputate ultrices neque.
+In quam magna, "placerat" id nunc eu, posuere egestas massa.
+Nam efficitur elit eget mi accumsan faucibus. Nunc augue nisi, sodales vitae purus at, mattis cursus turpis. 
+Sed et dictum nisl, non sagittis odio. Donec porttitor in ligula quis tempus.
+Vivamus ac lorem sit amet lectus pulvinar porta quis nec magna.
+Donec sed lacus nec ex 'pharetra' malesuada et sed magna.
+Sed dapibus dui sit amet dui sodales sollicitudin. Duis efficitur tristique ligula.
+Praesent id dolor id diam rhoncus vulputate. Ut ut leo ultricies, convallis ex in, maximus turpis. 
+Nullam lacinia dolor eu venenatis tincidunt. Vestibulum iaculis lectus tortor, id ultricies erat posuere vel.
+
+''';
+
+const _$embeddedJsonString = r'''
+{
+  "app_name": "MyWebApp",
+  "debug_mode": true,
+  "database": {
+    "host": "localhost",
+    "port": 5432,
+    "username": "myuser",
+    "password": "mypassword",
+    "database_name": "mydatabase"
+  },
+  "logging": {
+    "level": "info",
+    "file": "/var/log/mywebapp.log"
+  },
+  "cache": {
+    "enabled": true,
+    "expiry": 3600
+  },
+  "security": {
+    "secret_key": "somerandomstring",
+    "csrf_protection": true,
+    "cors_allowed_origins": ["https://example.com", "https://api.example.com"],
+    "jwt": {
+      "enabled": true,
+      "expiration": 3600,
+      "refresh_token_expiration": 604800,
+      "secret": "anothersecretkey"
+    }
+  },
+  "email": {
+    "smtp_host": "smtp.example.com",
+    "smtp_port": 587,
+    "smtp_username": "myemail@example.com",
+    "smtp_password": "mypassword",
+    "sender_email": "noreply@example.com"
+  },
+  "api_keys": {
+    "weather_api_key": "myweatherapikey",
+    "payment_gateway_api_key": "mypaymentgatewayapikey"
+  }
+}
+
+''';
+
+// **************************************************************************
+// LiteralEmbeddingGenerator
+// **************************************************************************
+
+const _$embeddedJson = (
+  appName: "MyWebApp",
+  debugMode: true,
+  database: (
+    host: "localhost",
+    port: 5432,
+    username: "myuser",
+    password: "mypassword",
+    databaseName: "mydatabase"
+  ),
+  logging: (level: "info", file: "/var/log/mywebapp.log"),
+  cache: (enabled: true, expiry: 3600),
+  security: (
+    secretKey: "somerandomstring",
+    csrfProtection: true,
+    corsAllowedOrigins: ["https://example.com", "https://api.example.com"],
+    jwt: (
+      enabled: true,
+      expiration: 3600,
+      refreshTokenExpiration: 604800,
+      secret: "anothersecretkey"
+    )
+  ),
+  email: (
+    smtpHost: "smtp.example.com",
+    smtpPort: 587,
+    smtpUsername: "myemail@example.com",
+    smtpPassword: "mypassword",
+    senderEmail: "noreply@example.com"
+  ),
+  apiKeys: (
+    weatherApiKey: "myweatherapikey",
+    paymentGatewayApiKey: "mypaymentgatewayapikey"
+  )
+);
+
+const _$embeddedToml = (
+  title: "TOML Example",
+  owner: (name: "Tom Preston-Werner", dob: "1979-05-27 07:32-08:00"),
+  database: (
+    enabled: true,
+    ports: [8000, 8001, 8002],
+    data: [
+      ["delta", "phi"],
+      [3.14]
+    ],
+    tempTargets: (cpu: 79.5, $case: 72.0)
+  ),
+  servers: (
+    alpha: (ip: "10.0.0.1", role: "frontend"),
+    beta: (ip: "10.0.0.2", role: "backend")
+  )
+);
+
+const _$embeddedYaml = (
+  name: "example",
+  publishTo: "none",
+  environment: (sdk: "^3.0.5"),
+  dependencies: (embedAnnotation: (path: "../embed_annotation")),
+  devDependencies: (
+    buildRunner: "^2.4.6",
+    lints: "^6.0.0",
+    embed: (path: "../embed")
+  ),
+  dependencyOverrides: (embedAnnotation: (path: "../embed_annotation"))
+);
+
+// **************************************************************************
+// BinaryEmbeddingGenerator
+// **************************************************************************
+
+const _$embeddedBinary = [
+  137,
+  80,
+  78,
+  71,
+  13,
+  10,
+  26,
+  10,
+  0,
+  0,
+  0,
+  13,
+  73,
+  72,
+  68,
+  82,
+  0,
+  0,
+  0,
+  10,
+  0,
+  0,
+  0,
+  10,
+  8,
+  2,
+  0,
+  0,
+  0,
+  2,
+  80,
+  88,
+  234,
+  0,
+  0,
+  0,
+  233,
+  73,
+  68,
+  65,
+  84,
+  24,
+  211,
+  37,
+  200,
+  49,
+  46,
+  131,
+  97,
+  0,
+  0,
+  208,
+  247,
+  85,
+  53,
+  90,
+  42,
+  33,
+  196,
+  64,
+  68,
+  132,
+  72,
+  147,
+  78,
+  238,
+  208,
+  5,
+  139,
+  196,
+  228,
+  22,
+  18,
+  119,
+  248,
+  15,
+  96,
+  195,
+  5,
+  44,
+  38,
+  155,
+  137,
+  201,
+  32,
+  149,
+  38,
+  157,
+  170,
+  12,
+  45,
+  66,
+  106,
+  104,
+  229,
+  87,
+  73,
+  181,
+  253,
+  12,
+  182,
+  151,
+  23,
+  98,
+  140,
+  66,
+  24,
+  187,
+  104,
+  59,
+  184,
+  225,
+  142,
+  75,
+  62,
+  150,
+  217,
+  166,
+  44,
+  27,
+  78,
+  199,
+  106,
+  35,
+  101,
+  52,
+  168,
+  114,
+  205,
+  35,
+  141,
+  13,
+  157,
+  10,
+  225,
+  108,
+  100,
+  147,
+  50,
+  115,
+  29,
+  170,
+  52,
+  184,
+  163,
+  78,
+  93,
+  38,
+  73,
+  66,
+  76,
+  174,
+  40,
+  125,
+  88,
+  175,
+  209,
+  228,
+  107,
+  133,
+  85,
+  214,
+  88,
+  253,
+  161,
+  23,
+  146,
+  248,
+  192,
+  27,
+  173,
+  41,
+  221,
+  45,
+  22,
+  89,
+  99,
+  161,
+  75,
+  135,
+  55,
+  33,
+  94,
+  38,
+  94,
+  73,
+  169,
+  84,
+  40,
+  144,
+  39,
+  199,
+  44,
+  51,
+  29,
+  66,
+  243,
+  62,
+  118,
+  115,
+  228,
+  255,
+  135,
+  194,
+  47,
+  61,
+  82,
+  250,
+  193,
+  79,
+  120,
+  138,
+  247,
+  243,
+  76,
+  51,
+  57,
+  160,
+  79,
+  74,
+  202,
+  55,
+  165,
+  18,
+  19,
+  33,
+  126,
+  143,
+  199,
+  194,
+  144,
+  97,
+  32,
+  75,
+  150,
+  48,
+  100,
+  144,
+  249,
+  231,
+  222,
+  238,
+  238,
+  252,
+  204,
+  96,
+  42,
+  182,
+  143,
+  15,
+  139,
+  71,
+  231,
+  75,
+  43,
+  197,
+  214,
+  206,
+  210,
+  205,
+  73,
+  255,
+  253,
+  229,
+  118,
+  191,
+  247,
+  249,
+  252,
+  7,
+  217,
+  107,
+  88,
+  158,
+  121,
+  56,
+  74,
+  159,
+  0,
+  0,
+  0,
+  0,
+  73,
+  69,
+  78,
+  68,
+  174,
+  66,
+  96,
+  130
+];
+
+const _$embeddedBinaryBase64 =
+    'iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAA6UlEQVQY0yXIMS6DYQAA0PdVNVoqIcRARIRIk07u0AWLxOQWEnf4D2DDBSwmm4nJIJUmnaoMLUJqaOVXSbX9DLaXF2KMQhi7aDu44Y5LPpbZpiwbTsdqI2U0qHLNI40NnQrhbGSTMnMdqjS4o05dJklCTK4ofViv0eRrhVXWWP2hF5L4wButKd0tFlljoUuHNyFeJl5JqVQokCfHLDMdQvM+dnPk/4fCLz1S+sFPeIr380wzOaBPSso3pRITIX6Px8KQYSBLljBkkPnn3u7u/MxgKraPD4tH50srxdbO0s1J//3ldr/3+fwH2WtYnnk4Sp8AAAAASUVORK5CYII=';
